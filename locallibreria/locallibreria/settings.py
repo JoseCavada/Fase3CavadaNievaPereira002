63e2b10524ee701575b0418704cf374e85052553
Glaser Cross <ro.nieva@alumnos.duoc.cl>
2020-11-28 02:26:42 -0300

    adelanto de secion

--- locallibreria/locallibreria/settings.py
+++ locallibreria/locallibreria/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 
 MEDIA_URL = '/media/'
 
--- locallibreria/locallibreria/urls.py
+++ locallibreria/locallibreria/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('catalog/', include('catalog.urls')),
+    path ('accounts/',include('django.contrib.auth.urls')),
 
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
